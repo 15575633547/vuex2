6616d79600baf49b8ef3b8b6ea2f0a24860eca62
citrus <2602533954@qq.com>
2021-01-21 20:06:46 +0800

    vuex2

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    {{$store.state.a.name}}
+    {{$store.state.b.name}}
+    {{$store.state.name}}
+    <br>
+    <button @click = "$store.commit('changeName')">changeName</button>
+    <br>
+     <button @click = "$store.commit('a/changeName')">changeNameA</button>
+     <br>
+      <button @click = "$store.commit('b/changeName')">changeNameB</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/store/a.js
@@ -0,0 +1,18 @@
+export default{
+    namespaced: true,
+    state: {
+        name:"小A"
+    },
+    mutations: {
+        changeName(state){
+            state.name="小小A"
+        
+        }
+    },
+    actions: {
+        
+    },
+    getters: {
+        
+    }
+};
